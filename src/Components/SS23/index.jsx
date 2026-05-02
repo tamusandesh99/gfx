1d1916607602921e0d5f840406acd3e4c5cd5107
Sandy Gurung <sandy.gurung@tierpoint.com>
2026-05-02 00:06:44 -0400

    push2

--- src/Components/SS23/index.jsx
+++ src/Components/SS23/index.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import './index.scss'; 
-import r1 from '../../../public/pics/roster1-png.png';
-import r2 from '../../../public/pics/roster2-png.png';
+import React from "react";
+import "./index.scss";
+import r1 from "../../../public/pics/roster1-png.png";
+import r2 from "../../../public/pics/roster2-png.png";
+import r3 from "../../../public/pics/meow.png";
+import r4 from "../../../public/pics/roster6-png.png";
+import pubgLogo from "../../../public/pics/pubg-logo-b.png";
+import sponsors from "../../../public/pics/sponsor-b.png";
+import ngclogo from "../../../public/pics/logo2.png";
+import ngclogoEsports from "../../../public/pics/logo1.png";
+import bg from "../../../public/pics/bg4.jpg";
 
 const SuperSunday = () => {
-  // Creating an array for the 18 teams (4-5-4-5 layout)
-  const teams = Array.from({ length: 18 }, (_, i) => i + 1);
+  // Defining the rows specifically to meet the 4-5-4-5 requirement
+  const rows = [4, 5, 4, 5];
 
   return (
     <div className="thumbnail-canvas">
       <div className="background-overlay">
         <div className="spotlight"></div>
-        <div className="embers"></div>
-      </div>
-
-      {/* Title Section */}
-      <div className="header-section">
         <div className="trophy-silhouette"></div>
+      </div>
+      <div className="bg-image" style={{ backgroundImage: `url(${bg})` }} />
+
+      <div className="bg-overlay" />
+      {/* Corner Logos */}
+      <img src={pubgLogo} alt="" className="corner tl" />
+      <img src={ngclogo} alt="" className="corner tr" />
+      <img src={sponsors} alt="" className="corner bl" />
+      <img src={ngclogoEsports} alt="" className="corner br" />
+
+      <div className="header-section">
         <h1 className="main-title">
-          <span className="silver">SUPER</span> <span className="gold">SUNDAY</span>
+          <span className="silver">SUPER</span>{" "}
+          <span className="gold">SUNDAY</span>
         </h1>
         <div className="branding">
           <span className="white-text">PRESENTED BY</span>
@@ -27,27 +41,34 @@ const SuperSunday = () => {
         </div>
       </div>
 
-      {/* Main Content Area */}
       <div className="content-wrap">
-        {/* Player Cutouts - Left */}
-        <div className="player player-tl"><img src={r1} alt="Player" /></div>
-        <div className="player player-bl"><img src={r2} alt="Player" /></div>
-
-        {/* Honeycomb Grid */}
-        <div className="honeycomb-container">
-            {teams.map((team) => (
-              <div key={team} className="hexagon">
-                <div className="hex-inner">
-                  {/* <img src={`/logos/team${team}.png`} alt="logo" /> */}
-                  <div className="logo-placeholder"></div>
-                </div>
-              </div>
-            ))}
+        {/* Players - Framing the grid */}
+        <div className="player-wrapper player-tl">
+          <img src={r1} alt="p1" />
+        </div>
+        <div className="player-wrapper player-bl">
+          <img src={r2} alt="p2" />
+        </div>
+        <div className="player-wrapper player-tr">
+          <img src={r3} alt="p3" />
+        </div>
+        <div className="player-wrapper player-br">
+          <img src={r4} alt="p4" />
         </div>
 
-        {/* Player Cutouts - Right */}
-        <div className="player player-tr"><img src={r1} alt="Player" /></div>
-        <div className="player player-br"><img src={r2} alt="Player" /></div>
+        <div className="honeycomb-grid">
+          {rows.map((count, rowIndex) => (
+            <div key={rowIndex} className={`hex-row row-${count}`}>
+              {[...Array(count)].map((_, i) => (
+                <div key={i} className="hexagon">
+                  <div className="hex-inner">
+                    <div className="logo-placeholder"></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
